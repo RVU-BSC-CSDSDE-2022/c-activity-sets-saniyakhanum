2267e45292fb1da634c0129fe1447b22ac46f136
Saniya Khanum  Khanum <>
2022-09-27 11:12:47 +0000

    solved problems

---
+++ set01/problem01.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(void){
+  printf ("Myself Saniya");
+  return 0;
+}
---
+++ set01/problem02.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void) {
+  int a,b,sum;
+  a=12,b=15;
+  sum=a+b;
+  printf("%d %d %d",a,b,sum);
+  return 0;
+  
+}
